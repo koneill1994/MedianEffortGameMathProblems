a2a6586f2e9b454c15bedfddfffb75e947a5e6d8
Kevin O'Neill <koneill1994@gmail.com>
2017-08-09 16:20:13 -0400

    Updating program to do more complex operations

--- math_problem.py
+++ math_problem.py
@@ -16,20 +16,62 @@ except NameError:
 no_of_levels = 7
 # determines the number of difficulty levels
 
-max_digits=3
+max_digits=2
 # the maximum number of digits in a single number
 
 max_terms=4
 # the maximum number of numbers one is asked to do operations on
 
 
-operations=['+','-','*','/']
+max_operations=['+','-','*','/']
+# different operations to be performed on the numbers, in order of increasing difficulty
 
 #######################
 
 def ScaleXtoY(x,xmax,ymax,ymin):
   return max(int(round((1.0*x/xmax)*ymax)),ymin)
 
+def char_to_operation(a,b,char):
+  if char == '+':
+    return a+b
+  elif char == '-':
+    return a-b
+  elif char == '*':
+    return a*b
+  elif char == '/':
+    return a/b
+  else:
+    return float('nan')
+
+
+
+def ComputeAnswer(nums,ops):
+  
+  if len(nums) not == len(ops)+1:
+    print("ERROR: Number of NUMS and OPS do not correspond")
+  
+  #interleave the two lists
+  eq=[]
+  
+  for x in range(ops):
+    eq.append(nums[x])
+    eq.append(ops[x])
+  eq.append(nums[-1])
+  
+  # nums are on the odd numbers, ops are on the evens
+  
+  # pemdas
+   
+  # m
+  mults=[]
+  for x in range(len(eq)):
+    if eq[x]=="*":
+      mults.append(x)
+  
+  ### this is a work in progress, find a way to make it work
+  
+  
+  
 while(1):
   
   while(1):
@@ -43,29 +85,34 @@ while(1):
   # generate equation & answer
   terms=ScaleXtoY(current_level,no_of_levels,max_terms,2)
   digits=ScaleXtoY(current_level,no_of_levels,max_digits,1)
+  operations=max_operations[:ScaleXtoY(current_level,no_of_levels,len(max_operations),1)]
   
-  number_sum=[]
+  numbers=[]
+  ops=[]
   
   for x in range(terms):
-    number_sum.append(random.randrange(-10**digits,10**digits))
+    numbers.append(random.randrange(0,10**digits))
+  
+  for x in range(terms-1):
+    ops.append(operations[random.randrange(0,len(operations))])
   
   
   s=""
-  for t in number_sum:
-    if t<0:
-      s=s[:-1]
-    s+=str(t)+"+"
+  for t in range(len(numbers)):
+    s+=str(numbers[t])
+    if t<len(ops):
+      s+=ops[t]
     
-  print(s[:-1])
+  print(s)
   
   
   answer = input("answer:")
   if answer=="":
     answer="0"
-  if int(answer)==sum(number_sum):
+  if int(answer)==sum(numbers):
     print("correct!")
   else:
-    print("incorrect, sum is "+str(sum(number_sum)))
+    print("incorrect, sum is "+str(sum(numbers)))
   
   
   
