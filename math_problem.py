c002cd704d66776d56dabd44385f63344e9c73b7
Kevin O'Neill <koneill1994@gmail.com>
2017-09-08 12:54:14 -0400

    Revert "Fixed up math solver code so it plays nice with otree"
    This reverts commit 90dfbef901fe90cf7c6e239b390fe4826ff8a3ad.
    
    # Conflicts:
    #	math_problem.py

--- math_problem.py
+++ math_problem.py
@@ -1,7 +1,11 @@
 # script which will generate a math problem of varying difficulty based on user input
 # Kevin O'Neill
 
+<<<<<<< HEAD
 import random
+=======
+import random, time, os
+>>>>>>> parent of 90dfbef... Fixed up math solver code so it plays nice with otree
 
 import mathsolver as ms
 
@@ -30,14 +34,69 @@ max_operations=['+','-','*','/']
 
 #######################
 
+<<<<<<< HEAD
 def ScaleXtoY(x,xmax,ymax,ymin):
   return max(int(round((1.0*x/xmax)*ymax)),ymin)
 
+=======
+
+def WriteHeader(logfile):
+  c=',' # for csv
+  s = ''
+  s+='subject_id'+c
+  s+='difficulty_level'+c
+  s+='time_when_presented'+c
+  s+='problem'+c
+  s+='correct_answer'+c
+  s+='subject_answer'+c
+  s+='time_when_answered'
+  logfile.write(s+'\n')
+
+def WriteLine(logfile,difficulty_level,presented_time,problem,correct_answer,subj_ans,answered_time):
+  c=',' # for csv
+  s=''
+  s+=str(subj_id)+c
+  s+=str(difficulty_level)+c
+  s+=str(presented_time)+c
+  s+=str(problem)+c
+  s+=str(correct_answer)+c
+  s+=str(subj_ans)+c
+  s+=str(answered_time)
+  logfile.write(s+'\n')
+
+def Create_Logfile(subj_id):
+  if not os.path.exists('./logs/'):
+    os.makedirs('./logs/')
+  f = open('./logs/logfile_' + subj_id + '.csv','w')
+  WriteHeader(f)
+  return f
+
+def ScaleXtoY(x,xmax,ymax,ymin):
+  return max(int(round((1.0*x/xmax)*ymax)),ymin)
+
+
+
+
+start_time = time.time()
+presented,answered=0,0
+
+subj_id = raw_input('enter subject id: ')
+
+logf=Create_Logfile(subj_id)
+
+  
+>>>>>>> parent of 90dfbef... Fixed up math solver code so it plays nice with otree
   
 while(1):
   
   while(1):
+<<<<<<< HEAD
     current_level=int(input("Difficulty Level? "))
+=======
+    lvl = input("Difficulty Level? ")
+    if lvl == '': lvl = 1
+    current_level=int(lvl)
+>>>>>>> parent of 90dfbef... Fixed up math solver code so it plays nice with otree
     if current_level<=0 or current_level >no_of_levels:
       print("error: difficulty level out of range")
       print("Allowed levels: 1 through "+str(no_of_levels))
@@ -66,11 +125,22 @@ while(1):
       s+=ops[t]
     
   print(s)
+<<<<<<< HEAD
+=======
+  eq=s
+  presented = time.time() - start_time
+  
+>>>>>>> parent of 90dfbef... Fixed up math solver code so it plays nice with otree
   
   correct_ans = ms.solve(s)
   
   
   answer = input("answer:")
+<<<<<<< HEAD
+=======
+  answered = time.time() - start_time
+  
+>>>>>>> parent of 90dfbef... Fixed up math solver code so it plays nice with otree
   if answer=="":
     answer="0"
   if int(answer)==correct_ans:
@@ -78,5 +148,13 @@ while(1):
   else:
     print("incorrect, sum is "+str(correct_ans))
   
+<<<<<<< HEAD
   
   
+=======
+  WriteLine(logf,current_level,presented,eq,correct_ans,answer,answered)
+  
+  
+  
+logf.close()
+>>>>>>> parent of 90dfbef... Fixed up math solver code so it plays nice with otree
