90dfbef901fe90cf7c6e239b390fe4826ff8a3ad
Kevin O'Neill <koneill1994@gmail.com>
2017-09-08 12:40:50 -0400

    Fixed up math solver code so it plays nice with otree

--- math_problem.py
+++ math_problem.py
@@ -1,15 +1,10 @@
 # script which will generate a math problem of varying difficulty based on user input
 # Kevin O'Neill
 
-import random, time, os
+import random, os
 
 import mathsolver as ms
 
-# python2 compatibility##
-try:
-   input = raw_input
-except NameError:
-   pass
 ########################
 
 ## parameters
@@ -24,70 +19,22 @@ max_digits=2
 max_terms=4
 # the maximum number of numbers one is asked to do operations on
 
-
 max_operations=['+','-','*','/']
 # different operations to be performed on the numbers, in order of increasing difficulty
+# all supported is ['+','-','*','/','^']
+
 
 #######################
 
 
-def WriteHeader(logfile):
-  c=',' # for csv
-  s = ''
-  s+='subject_id'+c
-  s+='difficulty_level'+c
-  s+='time_when_presented'+c
-  s+='problem'+c
-  s+='correct_answer'+c
-  s+='subject_answer'+c
-  s+='time_when_answered'
-  logfile.write(s+'\n')
-
-def WriteLine(logfile,difficulty_level,presented_time,problem,correct_answer,subj_ans,answered_time):
-  c=',' # for csv
-  s=''
-  s+=str(subj_id)+c
-  s+=str(difficulty_level)+c
-  s+=str(presented_time)+c
-  s+=str(problem)+c
-  s+=str(correct_answer)+c
-  s+=str(subj_ans)+c
-  s+=str(answered_time)
-  logfile.write(s+'\n')
-
-def Create_Logfile(subj_id):
-  if not os.path.exists('./logs/'):
-    os.makedirs('./logs/')
-  f = open('./logs/logfile_' + subj_id + '.csv','w')
-  WriteHeader(f)
-  return f
 
 def ScaleXtoY(x,xmax,ymax,ymin):
   return max(int(round((1.0*x/xmax)*ymax)),ymin)
 
 
-
-
-start_time = time.time()
-presented,answered=0,0
-
-subj_id = raw_input('enter subject id: ')
-
-logf=Create_Logfile(subj_id)
-
-  
-  
-while(1):
-  
-  while(1):
-    lvl = input("Difficulty Level? ")
-    if lvl == '': lvl = 1
-    current_level=int(lvl)
-    if current_level<=0 or current_level >no_of_levels:
-      print("error: difficulty level out of range")
-      print("Allowed levels: 1 through "+str(no_of_levels))
-    else:
-      break
+def GenerateEquationAndAnswer(lvl):
+  if lvl == '': lvl = 1
+  current_level=int(lvl)
   
   # generate equation & answer
   terms=ScaleXtoY(current_level,no_of_levels,max_terms,2)
@@ -110,26 +57,10 @@ while(1):
     if t<len(ops):
       s+=ops[t]
     
-  print(s)
   eq=s
-  presented = time.time() - start_time
   
   
   correct_ans = ms.solve(s)
   
-  
-  answer = input("answer:")
-  answered = time.time() - start_time
-  
-  if answer=="":
-    answer="0"
-  if int(answer)==correct_ans:
-    print("correct!")
-  else:
-    print("incorrect, sum is "+str(correct_ans))
-  
-  WriteLine(logf,current_level,presented,eq,correct_ans,answer,answered)
-  
-  
-  
-logf.close()
+  return (eq,correct_ans)
+
